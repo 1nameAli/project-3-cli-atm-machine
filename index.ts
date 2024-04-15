fcabc85a29e904ff4bfef3156c39a463d9f81048
Muhammad Ali <149589024+1nameAli@users.noreply.github.com>
2024-04-15 11:51:10 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,47 @@
+import { log } from "console";
+import inquirer from "inquirer";
+
+let myBalance = 5000000;
+let myPinCode = 2345;
+
+let pinAns = await inquirer.prompt([
+  {
+    name: "userEnteredPin",
+    type: "number",
+    message: "Enter you pin : ",
+  },
+]);
+if (pinAns.userEnteredPin === myPinCode) {
+  console.log("You entered correct pin code ");
+
+  let operations = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      message: "What operation you want to perform : ",
+      choices: ["withdraw", "checkbalance"],
+    },
+  ]);
+  if (operations.operation === "withdraw") {
+    let withdrawalAmount = await inquirer.prompt([
+      {
+        name: "amountEntered",
+        type: "list",
+        message: "Enter your desired amount : ",
+        choices: [10000,20000,30000],
+      },
+    ]);
+    if (myBalance >= withdrawalAmount.amountEntered) {
+     myBalance-=withdrawalAmount.amountEntered
+      console.log(
+        `You have withdraw ${withdrawalAmount.amountEntered}$ amount and your remaining balance is ${myBalance}$`
+      );
+    } else {
+      console.log("Insufficient Balance !");
+    }
+  } else if (operations.operation === "checkbalance") {
+    console.log(`Your total Balance is ${myBalance}`);
+  }
+} else {
+  console.log("Incorrect Pin code!");
+}
